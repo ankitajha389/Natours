29b9a8c9926aa7de93f94144eee7a64c71013891
ankitajha389 <ankitajha389@gmail.com>
2025-07-02 20:40:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const APIFeatures=require('./utils/apiError');
+const globalErrorHandler = require('./controllers/errorController');
+
+const app = express();
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`)); 
+
+app.use((req, res, next) => {
+  console.log('Hello from middleware 👋');
+  next();
+});
+
+// Add request timestamp
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// ROUTES
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+// Handle undefined routes (All other routes)
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Global Error Handling Middleware
+app.use(globalErrorHandler);
+
+module.exports = app;
