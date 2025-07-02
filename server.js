d3f818eb61c4d21bee0148a019ee558fdfbb8b29
ankitajha389 <ankitajha389@gmail.com>
2025-07-02 20:33:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB) 
+  .then(() => console.log('DB connection successful!'))
+  .catch(err => {
+    console.error('DB connection error:', err);
+    process.exit(1);
+  });
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+// Handle unhandled promise rejections (like DB connection fail)
+process.on('unhandledRejection', err => {
+  console.error('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.error(err.name, err.message);
+  server.close(() => process.exit(1));
+});
+
+// Handle uncaught exceptions (like coding errors)
+process.on('uncaughtException', err => {
+  console.error('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.error(err.name, err.message);
+  process.exit(1);
+});
+
+
+
+
+
+
+
+
+
